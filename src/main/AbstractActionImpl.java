df11c65b64fa114da2f28547d39c3fc0af8644d1
your name <1642984594@qq.com>
2018-08-13 23:29:02 +0800

    动态加载jar包的,测试了一下确实好使，再想想怎么做吧

---
+++ src/main/AbstractActionImpl.java
@@ -0,0 +1,8 @@
+////import classloader.forjar.AbstractAction;
+//
+//public class AbstractActionImpl {
+////    @Override
+//    public String toString(){
+//        return "this is the shixianlei";
+//    }
+//}
