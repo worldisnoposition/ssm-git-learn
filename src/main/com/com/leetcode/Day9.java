4f2be267f574c5cbd29033b72149a915129093aa
your name <1642984594@qq.com>
2018-09-09 22:48:13 +0800

    被一道中级题难到了，惭愧啊，算法题这是被虐啊

--- src/main/com/com/leetcode/Day9.java
+++ src/main/com/com/leetcode/Day9.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.*;
+
 public class Day9 {
     /**
      * 这么简单居然是个中级题，我写的也是够丑，够暴力
@@ -123,4 +125,107 @@ public class Day9 {
         }
         return result;
     }
+
+    /**
+     * 别人家的答案，顺便学习了一个Ararys.sort函数
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length-2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // 去掉重复的起点
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == 0) {
+                    result.add(Arrays.asList(new Integer[]{nums[i], nums[left], nums[right]}));
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++; // 去掉重复的左点
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--; // 去掉重复的右点
+                    right--; // 进入下一组左右点判断
+                    left++;
+                } else if (sum > 0) {
+                    right--; // sum>0 ,说明和过大了，需要变小，所以移动右边指针
+                } else {
+                    left++; // 同理，需要变大，移动左指针
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 中级题我居然没做出来，我的答案超时了，这个答案确实精巧，先排序，再用两个指针从左右向中间移动，减少比较次数
+     * @param nums
+     * @return
+     */
+     public List<List<Integer>> threeSum1(int[] nums) {
+         if(nums.length==0){
+             return new ArrayList();
+         }
+         int[][] temp = new int[nums.length-1][nums.length-1];
+         Map<Integer,Node> map = new HashMap();
+         List<List<Integer>> result = new ArrayList();
+         for(int i=0;i<nums.length-1;i++){
+             for(int j=i+1;j<nums.length;j++){
+                 int key = nums[i]+nums[j];
+                 Node node;
+                 if((node=map.get(key))==null){
+                     map.put(nums[i]+nums[j],nums[i]<nums[j]?new Node(i,j):new Node(j,i));
+                 }else{
+                     Node node1 = nums[i]<nums[j]?new Node(i,j):new Node(j,i);
+                     node1.next = node;
+                     map.put(key,node1);
+                 }
+             }
+         }
+         // System.out.println(map);
+         for(int k=0;k<nums.length;k++){
+             Node node;
+             if((node=map.get(-nums[k]))!=null){
+                 while(node!=null){
+                     if(k<node.i&&k<node.j){
+                         // System.out.print(k+"-"+node.i+"-"+node.j);
+                         List<Integer> toAdd = new ArrayList();
+                         if(nums[k]<nums[node.i]){
+                             toAdd.add(nums[k]);
+                             toAdd.add(nums[node.i]);
+                             toAdd.add(nums[node.j]);
+                         }else if(nums[k]>nums[node.j]){
+                             toAdd.add(nums[node.i]);
+                             toAdd.add(nums[node.j]);
+                             toAdd.add(nums[k]);
+                         }else{
+                             toAdd.add(nums[node.i]);
+                             toAdd.add(nums[k]);
+                             toAdd.add(nums[node.j]);
+                         }
+                         if(!result.contains(toAdd)){
+                             result.add(toAdd);
+                         }
+                     }
+                     node=node.next;
+                 }
+             }
+         }
+         return result;
+     }
+     class Node{
+         int i;
+         int j;
+         public Node(int i,int j){
+             this.i=i;
+             this.j=j;
+         }
+         Node next;
+         public String toString(){
+             return "{i="+i+",j="+j+",next="+this.next+"}";
+         }
+     }
 }
