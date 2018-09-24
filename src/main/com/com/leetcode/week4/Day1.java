b06e98f12a1d669b5a19c50a126964625d34d999
your name <1642984594@qq.com>
2018-09-24 15:27:50 +0800

    别人答案里copy出来的，肯定比我好，但是我觉得还有改进空间，写到代码注释里了

--- src/main/com/com/leetcode/week4/Day1.java
+++ src/main/com/com/leetcode/week4/Day1.java
@@ -160,3 +160,62 @@ public class Day1 {
         return res;
     }
 }
+
+/**
+ * 别人的答案就很好了，优雅又简洁，
+ * 但我觉得findLast方法其实还可以再改进下，可以不从0开始，而是从start开始
+ */
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[]{-1,-1};
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start+1<end) {
+            int mid = start+(end-start)/2;
+            if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        } else if (nums[end] == target) {
+            return end;
+        }
+
+        return -1;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start+1<end) {
+            int mid = start+(end-start)/2;
+            if (nums[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[end] == target) {
+            return end;
+        } else if (nums[start] == target) {
+            return start;
+        }
+
+        return -1;
+    }
+}
