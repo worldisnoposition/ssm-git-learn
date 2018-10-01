e5dce6ae1a1d18a73b75678b53fc40c20586c1e0
your name <1642984594@qq.com>
2018-10-01 23:40:53 +0800

    数独

--- src/main/com/com/leetcode/week5/Day1.java
+++ src/main/com/com/leetcode/week5/Day1.java
@@ -2,6 +2,7 @@ package com.leetcode.week5;
 
 public class Day1 {
     /**
+     * 数独
      * 一道中级题，其实思想很快就想好了，但是编码时总是又疏忽，运行异常，
      * 新版的leetCode编辑器真难用，影响我开发效率
      * 看了别人的代码我觉得我写的真的很简洁，循环次数也少
@@ -10,17 +11,19 @@ public class Day1 {
      */
     public boolean isValidSudoku(char[][] board) {
         int[][] cache = new int[27][9];
+        int temp = 0;
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.'){
                     continue;
                 }
-                if(cache[i+9][board[i][j]-49]==0
-                        &&cache[j+18][board[i][j]-49]==0
-                        &&cache[i/3+j/3*3][board[i][j]-49]==0){
-                    cache[i+9][board[i][j]-49]=1;
-                    cache[j+18][board[i][j]-49]=1;
-                    cache[i/3+j/3*3][board[i][j]-49]=1;
+                temp = board[i][j]-49;
+                if(cache[i+9][temp]==0
+                        &&cache[j+18][temp]==0
+                        &&cache[i/3+j/3*3][temp]==0){
+                    cache[i+9][temp]=1;
+                    cache[j+18][temp]=1;
+                    cache[i/3+j/3*3][temp]=1;
                 }else{
                     return false;
                 }
