e37bf2a8612f9d57a35dab110d0e3d20043e0f7a
your name <1642984594@qq.com>
2018-10-01 23:30:36 +0800

    一道中级题，其实思想很快就想好了，但是编码时总是又疏忽，运行异常， 新版的leetCode编辑器真难用，影响我开发效率 看了别人的代码我觉得我写的真的很简洁，循环次数也少

---
+++ src/main/com/com/leetcode/week5/Day1.java
@@ -0,0 +1,31 @@
+package com.leetcode.week5;
+
+public class Day1 {
+    /**
+     * 一道中级题，其实思想很快就想好了，但是编码时总是又疏忽，运行异常，
+     * 新版的leetCode编辑器真难用，影响我开发效率
+     * 看了别人的代码我觉得我写的真的很简洁，循环次数也少
+     * @param board
+     * @return
+     */
+    public boolean isValidSudoku(char[][] board) {
+        int[][] cache = new int[27][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                if(cache[i+9][board[i][j]-49]==0
+                        &&cache[j+18][board[i][j]-49]==0
+                        &&cache[i/3+j/3*3][board[i][j]-49]==0){
+                    cache[i+9][board[i][j]-49]=1;
+                    cache[j+18][board[i][j]-49]=1;
+                    cache[i/3+j/3*3][board[i][j]-49]=1;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
