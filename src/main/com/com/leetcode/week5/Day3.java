ff7bb4b83416897691c8e8335c04a9818e929bab
your name <1642984594@qq.com>
2018-10-03 23:31:42 +0800

    一道中级题，我感觉这道题，还是能找到一些做题技巧的了，简单的递归和动态规划吧

---
+++ src/main/com/com/leetcode/week5/Day3.java
@@ -0,0 +1,42 @@
+package com.leetcode.week5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day3 {
+    /**
+     * 一道中级题，我感觉这道题，还是能找到一些做题技巧的了
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(candidates.length==0){
+            return new ArrayList();
+        }
+        Arrays.sort(candidates);
+        return combinationSum1(candidates,0,target);
+    }
+    public List<List<Integer>> combinationSum1(int[] candidates, int start, int target){
+        List<List<Integer>> result = new ArrayList();
+        for(int i=start;i<candidates.length;i++){
+            if(candidates[i]<target){
+                List<List<Integer>> temp = combinationSum1(candidates,i,target-candidates[i]);
+                for(int j=0;j<temp.size();j++){
+                    List<Integer> add = new ArrayList();
+                    add.add(candidates[i]);
+                    add.addAll(temp.get(j));
+                    result.add(add);
+                }
+            }else if(candidates[i]==target){
+                List<Integer> add = new ArrayList();
+                add.add(candidates[i]);
+                result.add(add);
+            }else{
+                break;
+            }
+        }
+        return result;
+    }
+}
