582beeff29c65445637c4c3e7592daaceedb2c72
your name <1642984594@qq.com>
2018-10-09 23:50:29 +0800

    这个就是别人的答案，用了一个数组标记用过的下标，一个做返回值，一个中间的list，一个原始的入参数组，共计4个参数

--- src/main/com/com/leetcode/week6/Day2.java
+++ src/main/com/com/leetcode/week6/Day2.java
@@ -48,3 +48,27 @@ public class Day2 {
 
 
 }
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0) return res;
+        help(nums, new ArrayList<>(), new boolean[nums.length], res);
+        return res;
+    }
+
+    public void help(int [] nums, List<Integer> temp, boolean [] used, List<List<Integer>> res){
+        if(temp.size() == nums.length){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(used[i]) continue;
+            used[i] = true;
+            temp.add(nums[i]);
+            help(nums, temp, used, res);
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+}
