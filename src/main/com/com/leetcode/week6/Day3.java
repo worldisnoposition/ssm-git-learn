359bcae848cb0739efd676fc591b138ab8c5d999
your name <1642984594@qq.com>
2018-10-10 21:54:53 +0800

    再数组中找到最小的缺失的正整数，一道hard题，本来一点思路没有，上网找了找答案，看了一会就明白，其实很简单，思路有了，一会就做出来了

---
+++ src/main/com/com/leetcode/week6/Day3.java
@@ -0,0 +1,25 @@
+package com.leetcode.week6;
+
+public class Day3 {
+
+    /**
+     * 一道hard题，本来一点思路没有，上网找了找答案，看了一会就明白，其实很简单，思路有了，一会就做出来了
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositive(int[] nums) {
+        int[] temp = new int[nums.length+1];
+        int result = nums.length;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=nums.length&&nums[i]>0){
+                temp[nums[i]]=1;
+            }
+        }
+        for(int i=1;i<temp.length;i++){
+            if(temp[i]!=1){
+                return i;
+            }
+        }
+        return nums.length+1;
+    }
+}
