2910a1e4ed3735352f870019e709842a685ae078
your name <1642984594@qq.com>
2018-11-14 22:38:33 +0800

    很简单的中级题，凑个提交，今天还是荒废了时间，剩下的时间看看springcload的书吧

---
+++ src/main/com/com/leetcode/week10/Day3.java
@@ -0,0 +1,34 @@
+package com.leetcode.week10;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Day3 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode first = head;
+        ListNode now = head;
+        int length = 1;
+        while (now.next != null) {
+            length++;
+            now = now.next;
+        }
+        int a = k % length;
+        int b = 0;
+        for (int i = 0; i < length - 1 - a; i++) {
+            head = head.next;
+        }
+        now.next = first;
+        first = head.next;
+        head.next = null;
+        return first;
+    }
+}
