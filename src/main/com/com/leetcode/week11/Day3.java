7ad54463af0573d0d0f051db50ecaced625d51e1
your name <1642984594@qq.com>
2018-11-21 23:01:39 +0800

    数组，加一，也很简单的题

--- src/main/com/com/leetcode/week11/Day3.java
+++ src/main/com/com/leetcode/week11/Day3.java
@@ -20,4 +20,35 @@ public class Day3 {
         }
         return grid[grid.length-1][grid[0].length-1];
     }
+
+    /**
+     * 数组，加一，也很简单的题
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        int next = 0;
+        for(int i = digits.length-1;i>=0;i--){
+            if(i==digits.length-1){
+                digits[i]++;
+            }else{
+                digits[i]+=next;
+            }
+            if(digits[i]==10){
+                next = 1;
+                digits[i]=0;
+            }else{
+                next= 0;
+            }
+        }
+        if(next == 1){
+            int[] result = new int[digits.length+1];
+            result[0]=1;
+            for(int i=1;i<result.length;i++){
+                result[i]=digits[i-1];
+            }
+            return result;
+        }
+        return digits;
+    }
 }
