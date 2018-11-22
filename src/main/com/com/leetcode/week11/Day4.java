e0bb1a8bc043c4c12198432894f9ca6788f7668d
your name <1642984594@qq.com>
2018-11-22 23:15:46 +0800

    又是一个简单题，和斐波那契数列差不多

--- src/main/com/com/leetcode/week11/Day4.java
+++ src/main/com/com/leetcode/week11/Day4.java
@@ -52,4 +52,21 @@ public class Day4 {
             }
         }
     }
+
+    /**
+     * 又是一个简单题，和斐波那契数列差不多
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if(n<=2)
+            return n;
+        int[] t = new int[n];
+        t[0]=1;
+        t[1]=2;
+        for(int i=2;i<n;i++){
+            t[i]=t[i-1]+t[i-2];
+        }
+        return t[n-1];
+    }
 }
