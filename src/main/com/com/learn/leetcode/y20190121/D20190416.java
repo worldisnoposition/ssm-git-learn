f6c8f696350800cbb3fab9e8e2f040a2e3d3b0bf
your name <1642984594@qq.com>
2019-04-16 23:07:55 +0800

    非常easy的easy题，一气呵成，2分钟搞定

---
+++ src/main/com/com/learn/leetcode/y20190121/D20190416.java
@@ -0,0 +1,36 @@
+package com.learn.leetcode.y20190121;
+
+public class D20190416 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p != null && q != null) {
+                if (p.val == q.val) {
+                    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+                }
+            }
+            return false;
+        }
+    }
+}
