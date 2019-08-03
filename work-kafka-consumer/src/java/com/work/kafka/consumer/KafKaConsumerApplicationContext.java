d5a5868fa1595b3a2957b9aee0230b86e2153e6f
lin3090 <15904033090@163.com>
2019-08-03 19:04:11 +0800

    kafka

--- work-kafka-consumer/src/java/com/work/kafka/consumer/KafKaConsumerApplicationContext.java
+++ work-kafka-consumer/src/java/com/work/kafka/consumer/KafKaConsumerApplicationContext.java
@@ -3,14 +3,18 @@ package com.work.kafka.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 //@EnableAutoConfiguration
 @SpringBootConfiguration
 @Configuration
-//@ImportResource(locations={"classpath:application.properties"})
-@SpringBootApplication(scanBasePackages = "com.work.kafka")
-public class KafkaConsumerApplicationContext {
+@ImportResource(locations={"classpath:applicationContext.xml"})
+@SpringBootApplication(scanBasePackages = "com.spider.zhiye")
+//@EnableJpaRepositories(basePackages = "com.spider.zhiye.jpa.repository")
+@EntityScan(basePackages = "com.spider.zhiye.jpa.entity")
+public class KafKaConsumerApplicationContext {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(KafKaConsumerApplicationContext.class,args);
         Thread.sleep(100000000L);
---
+++ work-kafka-producer/src/java/com/work/kafka/TestReflect.java
@@ -0,0 +1,9 @@
+package com.work.kafka;
+
+import lombok.Data;
+
+@Data
+public class TestReflect {
+    public String a;
+    private String b;
+}
